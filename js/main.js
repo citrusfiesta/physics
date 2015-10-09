1d5a14bc900582288cf53f753cf3ac005a238ea0
citrusfiesta <citrusfiesta@gmail.com>
2015-10-09 15:29:53 +0200

    add standardized update system

--- js/main.js
+++ js/main.js
@@ -11,21 +11,37 @@ requirejs([
 ) {
     'use strict';
 
-    var isRunning = false,
-        ballMan = new BallManager(),
-        updateLoop = function () {
-
+    var canvas = document.getElementById('canvas'),
+        ctx = canvas.getContext('2d'),
+        i = 0,
+        len = 0,
+        isRunning = false,
+        isPaused = false,
+        updateArr = [],
+        mainLoop = function () {
             if (!isRunning)
                 return;
-            console.log('updateLoop running');
-            requestAnimationFrame(updateLoop);
+            update();
+            requestAnimationFrame(mainLoop);
         },
         run = function () {
             if (!isRunning) {
                 isRunning = true;
-                updateLoop();
+                mainLoop();
+            }
+        },
+        update = function () {
+            for (i = 0, len = updateArr.length; i < len; i++) {
+                // preventing errors
+                if (!updateArr[i]) {
+                    console.log('Index ' + i + ' in updateArr returns null.');
+                    continue;
+                }
+
+                if (updateArr[i].update && !isPaused)
+                    updateArr[i].update();
             }
         };
-    ballMan.createBalls();
+    updateArr.push(new BallManager());
     run();
 });
--- js/managers/ballmanager.js
+++ js/managers/ballmanager.js
@@ -11,15 +11,19 @@ define('managers/ballmanager', [
     return function () {
         var i,
             balls = [],
+            createBalls = function (amount) {
+                if (!amount)
+                    amount = 1;
+                for (i = 0; i < amount; i++) {
+                    console.log(amount - i);
+                    balls.push(new Ball());
+                }
+            },
             ballMan = {
-                createBalls: function (amount) {
-                    if (!amount)
-                        amount = 1;
-                    for (i = 0; i < amount; i++)
-                        console.log(amount - i);
+                update: function () {
+                    console.log('updating from ballMan');
                 }
             };
-
         return ballMan;
     };
 });
