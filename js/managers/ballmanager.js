8ef0366d1848647ede1608c0f7913b46f83a2696
citrusfiesta <citrusfiesta@gmail.com>
2015-10-22 16:24:10 +0200

    add new ball spawning functionality

--- js/managers/ballmanager.js
+++ js/managers/ballmanager.js
@@ -1,43 +1,70 @@
 define('managers/ballmanager', [
     'c',
-    'ball'
+    'ball',
+    'vector2'
 ], function (
     C,
-    Ball
+    Ball,
+    Vector2
 ) {
     'use strict';
     /**
-     * Handles all the balls
+     * Handles the spawning of the balls
      * @return {Object} ballMan Instance of BallManager
      */
-    return function () {
+    return function (objectManager) {
         var i = 0,
             len = 0,
-            ctx = C.ctx,
-            canvasWidth = C.canvas.width,
-            canvasHeight = C.canvas.height,
-            balls = [],
-            createBalls = function (amount) {
-                if (!amount)
-                    amount = 1;
-                for (i = 0; i < amount; i++) {
-                    console.log(amount - i);
-                    balls.push(new Ball());
-                }
+            objMan = objectManager,
+            mouseX = 0,
+            mouseY = 0,
+            clicking = false,
+            keys = [],
+            createBall = function (x, y, velocity, radius) {
+                objMan.add(new Ball(x, y, velocity, radius));
+            },
+            /**
+             * Spawns ball at mouse position with a randomized upward velocity
+             */
+            erupt = function () {
+                createBall(mouseX, mouseY, new Vector2(
+                    Math.random() * -4 + 2, //add -2 to 2 horizontal velocity
+                    Math.random() * -5 - 3 //add 3 to 8 upward velocity
+                ));
+            },
+            /**
+             * Spawns a ball at the current mouse position
+             */
+            drip = function () {
+                createBall(mouseX, mouseY);
             },
             ballMan = {
                 update: function () {
-                    // clear screen before redrawing
-                    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-
-                    for (i = 0, len = balls.length; i < len; i++) {
-                        balls[i].update();
-                        balls[i].draw();
-                    }
+                    if (clicking)
+                        erupt();
+                    if (keys[32]) //space bar
+                        drip();
+                },
+                keyDown: function (e) {
+                    keys[e.keyCode] = true;
+                    if (e.keyCode === 81) //Q
+                        objMan.clearBalls();
+                },
+                keyUp: function (e) {
+                    keys[e.keyCode] = false;
+                },
+                mouseDown: function (e) {
+                    clicking = true;
+                },
+                mouseUp: function (e) {
+                    clicking = false;
+                },
+                mouseMove: function (e) {
+                    mouseX = e.clientX;
+                    mouseY = e.clientY;
                 }
             };
 
-        createBalls(200);
         return ballMan;
     };
 });
